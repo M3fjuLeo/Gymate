f12acbfbb8130e9936cc66d031acc9da3c9bbd5b
M3fjuLeo <mateuszropek@onet.pl>
2024-04-30 14:10:25 +0200

    test

--- src/pages/Classes/ClassTile.jsx
+++ src/pages/Classes/ClassTile.jsx
@@ -4,7 +4,8 @@ import meditationImg from '../../../public/images/classesImg/meditation.jpg';
 function ClassTile({ img, title, trainer, time }) {
   return (
     <div
-      className={`relative bg-${img} flex h-[30rem] flex-col rounded-2xl bg-cover bg-center shadow-xl`}
+      className={`relative flex h-[30rem] flex-col rounded-2xl bg-cover bg-center shadow-xl`}
+      style={{ backgroundImage: img }}
     >
       <div className="absolute left-0 top-0 z-20 h-full w-full rounded-2xl bg-[#0000002a]">
         <div className="absolute bottom-10 left-10">
