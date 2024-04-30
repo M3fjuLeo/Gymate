7b47de4cd60bab8522709344bd8819a0f7fad880
M3fjuLeo <mateuszropek@onet.pl>
2024-04-30 13:48:14 +0200

    test

--- src/pages/About/AboutTiles.jsx
+++ src/pages/About/AboutTiles.jsx
@@ -1,4 +1,7 @@
 import AboutTile from './AboutTile';
+import progresionImg from '../../../public/images/who-we-are/progresion.png';
+import workoutImg from '../../../public/images/who-we-are/workout.png';
+import nutritionsImg from '../../../public/images/who-we-are/nutritions.png';
 
 function AboutTiles() {
   return (
@@ -7,19 +10,19 @@ function AboutTiles() {
         title="progression"
         description="Our team of experts will work with you to create a customized plan that
         helps you achieve success one step at a time."
-        img="public/images/who-we-are/progresion.png"
+        img={progresionImg}
       />
       <AboutTile
         title="workout"
         description="With a variety of workouts to choose from, you'll have everything you
            need to get into the best shape of your life"
-        img="public/images/who-we-are/workout.png"
+        img={workoutImg}
       />
       <AboutTile
         title="nutritions"
         description="Our team will work with you to create a personalized meal plan that
         helps you reach your specific health goals."
-        img="public/images/who-we-are/nutritions.png"
+        img={nutritionsImg}
       />
     </div>
   );
