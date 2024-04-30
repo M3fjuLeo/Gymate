ad8ee0b4bc17af85a142c07793333a62e2a53b9a
M3fjuLeo <mateuszropek@onet.pl>
2024-04-30 13:44:47 +0200

    test

--- src/pages/About/AboutTile.jsx
+++ src/pages/About/AboutTile.jsx
@@ -1,7 +1,7 @@
 function AboutTile({ title, description, img }) {
   return (
     <div
-      className={`hover:bg-aboutTileBgImage flex cursor-pointer flex-col items-center justify-center rounded-br-[35px] rounded-tl-[35px] bg-white px-10 py-16 text-center shadow-2xl duration-300 hover:text-white`}
+      className={`about-hover-tile flex cursor-pointer flex-col items-center justify-center rounded-br-[35px] rounded-tl-[35px] bg-white px-10 py-16 text-center shadow-2xl duration-300 hover:text-white`}
     >
       <img className="mb-4" src={img} alt="progression image" />
       <h2 className="mb-6 text-2xl font-bold uppercase">{title}</h2>
