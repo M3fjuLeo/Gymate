a8cb667cbc4a1b4e18f0f8cc5f3a7b8555a9bdcd
M3fjuLeo <mateuszropek@onet.pl>
2024-04-30 13:02:32 +0200

    test

--- src/ui/Title.jsx
+++ src/ui/Title.jsx
@@ -1,9 +1,11 @@
+import titleBg from '../../public/images/features/title-bg.svg';
+
 function Title({ title, width }) {
   return (
     <div className="flex items-center justify-center">
       <img
         className={`absolute ${width}`}
-        src="../../public/images/features/title-bg.svg"
+        src={titleBg}
         alt="title background image"
       />
       <h3 className="relative z-10 font-bold uppercase text-white">{title}</h3>
