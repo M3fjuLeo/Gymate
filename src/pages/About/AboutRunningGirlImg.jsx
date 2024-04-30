83bf36bf17600e5353c547603883ce19172a6c95
M3fjuLeo <mateuszropek@onet.pl>
2024-04-30 13:41:29 +0200

    test

--- src/pages/About/AboutRunningGirlImg.jsx
+++ src/pages/About/AboutRunningGirlImg.jsx
@@ -1,22 +1,15 @@
+import girlImg from '../../../public/images/who-we-are/girl-run.png';
+import bgImg from '../../../public/images/who-we-are/girl-redbg.png';
+import textImg from '../../../public/images/who-we-are/girl-side-text.png';
+import windImg from '../../../public/images/who-we-are/wind.png';
+
 function AboutRunningGirlImg() {
   return (
     <div className="relative flex w-full items-center md1000:hidden">
-      <img
-        className="absolute z-50"
-        src="public/images/who-we-are/girl-run.png"
-      />
-      <img
-        className="absolute right-[22%] w-[40%]"
-        src="public/images/who-we-are/girl-redbg.svg"
-      />
-      <img
-        className="absolute w-[17%]"
-        src="public/images/who-we-are/girl-side-text.png"
-      />
-      <img
-        className="absolute left-[55%] top-60 z-40 w-14"
-        src="public/images/who-we-are/wind.png"
-      />
+      <img className="absolute z-50" src={girlImg} />
+      <img className="absolute right-[22%] w-[40%]" src={bgImg} />
+      <img className="absolute w-[17%]" src={textImg} />
+      <img className="absolute left-[55%] top-60 z-40 w-14" src={windImg} />
     </div>
   );
 }
