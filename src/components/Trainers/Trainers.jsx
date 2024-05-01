8b3d166e25d9e8e638d8fe3f4f0152f55bba49c0
M3fjuLeo <mateuszropek@onet.pl>
2024-05-01 10:54:39 +0200

    test

--- src/components/Trainers/Trainers.jsx
+++ src/components/Trainers/Trainers.jsx
@@ -3,7 +3,7 @@ import Title from '../../ui/Title';
 
 function Trainers() {
   return (
-    <section className="bg-trainersBg page-padding bg-cover bg-center">
+    <section className="trainersSection page-padding bg-cover bg-center">
       <div className="container flex items-center justify-center py-28">
         <div className="flex flex-col items-center gap-8">
           <div className="flex w-[60%] flex-col items-center gap-4 text-center">
