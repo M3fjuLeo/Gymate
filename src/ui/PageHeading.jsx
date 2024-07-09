f259944f2feb0a0cede5fee40aa5b9d6c36f8c46
M3fjuLeo <mateuszropek@onet.pl>
2024-07-09 11:58:04 +0200

    responsive update

--- src/ui/PageHeading.jsx
+++ src/ui/PageHeading.jsx
@@ -1,6 +1,6 @@
 function PageHeading({ title }) {
   return (
-    <div className="pageHeadingBg flex h-[28rem] w-full flex-col items-center justify-end bg-cover bg-center pb-10">
+    <div className="pageHeadingBg flex h-[28rem] w-full flex-col items-center justify-end bg-cover bg-center pb-10 md1200:h-[12rem]">
       <h1 className=" text-4xl font-bold text-white">{title}</h1>
     </div>
   );
